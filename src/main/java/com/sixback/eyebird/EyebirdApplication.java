3cb9a395468122a933497ad6da1d73e27dcfbc54
조창래 <crcho5133@naver.com>
2024-01-22 20:06:52 +0900

    Add: BE templates

---
+++ src/main/java/com/sixback/eyebird/EyebirdApplication.java
@@ -0,0 +1,13 @@
+package com.sixback.eyebird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyebirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyebirdApplication.class, args);
+	}
+
+}
