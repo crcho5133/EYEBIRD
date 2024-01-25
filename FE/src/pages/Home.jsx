b8975497dd418257208243e6b8e1108470b395e9
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-01-26 02:51:34 +0900

    Feat : login modal창 Rodal로 변경

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import LobbyBtn from '@/components/lobby/LobbyBtn';
-import LoginFormModal from '@/components/modal/LoginFormModal';
+import React, { useState } from "react";
+import LobbyBtn from "@/components/lobby/LobbyBtn";
+import LoginFormModal from "@/components/modal/LoginFormModal";
 
 const Home = () => {
-  const [modalType, setModalType] = useState(null);
+  const [isModalVisible, setModalVisible] = useState(false);
 
-  const showLoginModal = () => setModalType('login');
-  const closeModal = () => setModalType(null);
+  const showRodal = () => {
+    setModalVisible(true);
+  };
+
+  const hideRodal = () => {
+    setModalVisible(false);
+  };
 
   return (
     <>
-      <LobbyBtn text="로그인" onClick={showLoginModal} className="mr-4" />
-
-      {modalType === 'login' && (
-        <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
-          <LoginFormModal closeModal={closeModal} />
-        </div>
-      )}
+      <LobbyBtn text="로그인" onClick={showRodal} className="mr-4" />
+      <LoginFormModal visible={isModalVisible} onClose={hideRodal} />
     </>
   );
 };
