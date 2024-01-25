5f07aff82575002f380b68058a1b77e0f38a3cd0
조창래 <crcho5133@naver.com>
2024-01-25 22:12:38 +0900

    Feat: 점수 변동 request dto 생성

--- src/main/java/com/sixback/eyebird/api/dto/PointReqDto.java
+++ src/main/java/com/sixback/eyebird/api/dto/PointReqDto.java
@@ -1,2 +1,11 @@
-package com.sixback.eyebird.api.dto;public class PointReqDto {
+package com.sixback.eyebird.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PointReqDto {
+    private int classicPt;
+    private int itemPt;
 }
