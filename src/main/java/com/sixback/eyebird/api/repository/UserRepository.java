48f68db696838bd078135651ba140da0fd23a3d2
조창래 <crcho5133@naver.com>
2024-01-25 22:19:18 +0900

    Refactor: 주어진 이메일을 지닌 유저, 주어진 닉네임을 지닌 유저가 존재하는지 확인

--- src/main/java/com/sixback/eyebird/api/repository/UserRepository.java
+++ src/main/java/com/sixback/eyebird/api/repository/UserRepository.java
@@ -9,6 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
     Boolean existsByEmail(String email);
 
-
+    Boolean existsByNickname(String nickname);
 
 }
