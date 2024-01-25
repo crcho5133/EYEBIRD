2f0347397eef5a7279f7644257a70380c57ebf75
조창래 <crcho5133@naver.com>
2024-01-25 10:53:12 +0900

    Refactor: JPA auditing 활성화

--- src/main/java/com/sixback/eyebird/EyebirdApplication.java
+++ src/main/java/com/sixback/eyebird/EyebirdApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA auditing 활성화
 public class EyebirdApplication {
 
 	public static void main(String[] args) {
