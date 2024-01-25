05ccbd1f4ba1a94272898952f2f288f29f5c95a4
sojugood <tkddlrtla@naver.com>
2024-01-26 02:38:54 +0900

    Feat: Game 페이지, 대기방 컴포넌트화(기존 코드 분리)

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,20 @@
-import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
 import Lobby from "./pages/Lobby";
 import Game from "./pages/Game";
+import Room from "./pages/Room";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ToastContainer stacked pauseOnFocusLoss={false} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lobby" element={<Lobby />} />
+          <Route path="/room" element={<Room />} />
           <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
