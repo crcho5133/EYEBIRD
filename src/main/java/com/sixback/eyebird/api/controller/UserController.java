82de8ee1b832edd40e9f4f5895bf473a72d73d63
조창래 <crcho5133@naver.com>
2024-01-25 22:10:27 +0900

    Feat: 회원수정, 닉네임 중복확인, 이메일 중복확인, 비밀번호 일치확인, 회원삭제

--- src/main/java/com/sixback/eyebird/api/controller/UserController.java
+++ src/main/java/com/sixback/eyebird/api/controller/UserController.java
@@ -1,29 +1,89 @@
 package com.sixback.eyebird.api.controller;
 
-import com.sixback.eyebird.api.dto.SignupReqDto;
+import com.sixback.eyebird.api.dto.*;
 import com.sixback.eyebird.api.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor // 의존성 주입
 @RequestMapping("/api/user")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid SignupReqDto signupReqDto) {
-        Long userId = userService.signup(signupReqDto);
-        if (userId == -1L) { // 중복으로 유저 생성 실패
-            // TODO 에러 발생으로 수정
-            return ResponseEntity.status(422).build();
-        }
-        return ResponseEntity.status(201).body(userId);
+    public ResponseEntity<SignupResDto> signup(@RequestBody @Valid SignupReqDto signupReqDto) {
+        String email = userService.signup(signupReqDto);
+        SignupResDto signupResDto = SignupResDto.builder()
+                .email(email)
+                .build();
+        return ResponseEntity.status(201).body(signupResDto);
     }
+
+    // 회원수정
+    @PatchMapping("")
+    public ResponseEntity<UpdateUserResDto> update(@RequestBody @Valid UpdateUserReqDto modifyUserDto, Authentication authentication) {
+        String email = authentication.getName();
+        userService.update(modifyUserDto, email);
+
+        UpdateUserResDto updateUserResDto = UpdateUserResDto.builder().
+                email(email)
+                .message("회원수정 성공: " + email + "의 개인정보를 수정했습니다")
+                .build();
+
+        return ResponseEntity.status(200).body(updateUserResDto);
+    }
+
+    // 닉네임 중복확인
+    @GetMapping("/check/nickname")
+    public ResponseEntity<CheckDuplicateResDto> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
+        Boolean existsDuplicateNickname = userService.existsNickname(nickname);
+        CheckDuplicateResDto checkDuplicateResDto = CheckDuplicateResDto.builder()
+                .check(!existsDuplicateNickname)
+                .build();
+
+        return ResponseEntity.status(200).body(checkDuplicateResDto);
+    }
+
+    @GetMapping("/check/email")
+    public ResponseEntity<CheckDuplicateResDto> checkDuplicateEmail(@RequestParam("email") String email) {
+        boolean existsDuplicateEmail = userService.existsEmail(email);
+        CheckDuplicateResDto checkDuplicateResDto = CheckDuplicateResDto.builder()
+                .check(!existsDuplicateEmail)
+                .build();
+
+        return ResponseEntity.status(200).body(checkDuplicateResDto);
+    }
+
+    @PostMapping("/check/password")
+    public ResponseEntity<Void> checkPassword(@RequestBody CheckPasswordReqDto checkPasswordReqDto, Authentication authentication) {
+        String password = checkPasswordReqDto.getPassword();
+
+        // 현재 로그인 된 유저의 이메일
+        String email = authentication.getName();
+
+        boolean matchPassword = userService.matchPassword(password, email);
+
+        if (matchPassword) {
+            return ResponseEntity.status(200).build();
+        }
+        else {
+            return ResponseEntity.status(401).build();
+        }
+
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<Void> delete(Authentication authentication) {
+        String email = authentication.getName();
+        userService.deleteUser(email);
+        return ResponseEntity.status(200).build();
+    }
+
 }
