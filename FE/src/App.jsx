7934b92f0ac2c84d1838df32406782bc50ffc722
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-01-28 14:51:45 +0900

    Feat:webMobileLayout 반영

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,18 @@
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
+import WebMobileLayout from "@/layouts/WebMobileLayout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lobby" element={<Lobby />} />
-        </Routes>
+        <WebMobileLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lobby" element={<Lobby />} />
+          </Routes>
+        </WebMobileLayout>
       </BrowserRouter>
     </>
   );
