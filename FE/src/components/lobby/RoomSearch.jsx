f78980c59dc43ebf871ddc62c958f9b51ee73d12
jius <tjqmone@naver.com>
2024-01-29 17:34:19 +0900

    Refactoring : 방찾기 추가 수정 건. 01.29

--- FE/src/components/lobby/RoomSearch.jsx
+++ FE/src/components/lobby/RoomSearch.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import NavBar from "./NavBar";
 
 const RoomSearch = () => {
   const [refresh, setRefresh] = useState(false);
@@ -45,79 +46,83 @@ const RoomSearch = () => {
   }, [refresh]);
 
   return (
-    <div className="App">
-      <header className="flex justify-between p-4 bg-blue-500 text-white">
-        <h1>방목록</h1>
-        <div className="relative">
-          <button onClick={handleButtonClick} className="bg-green-500 p-2 rounded">
-            {tabName}
-          </button>
-          {showMenu && (
-            <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-              <div
-                className="py-1"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="options-menu"
-              >
-                <button
-                  onClick={() => handleItemClick("아이템")}
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  role="menuitem"
-                >
-                  아이템전
-                </button>
-                <button
-                  onClick={() => handleItemClick("클래식")}
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  role="menuitem"
-                >
-                  클래식전
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-      </header>
-      <main className="p-4">
-        <h2 className="text-2xl mb-4">게임 방 리스트</h2>
-        <div className="flex gap-40">
-          <p>공개 여부</p>
-          <p>방제목</p>
-          <p>인원</p>
-          <p>방장</p>
-        </div>
-        {currentRooms.map((room) => (
-          <div key={room.roomName} className="border p-2 mb-2 flex gap-40">
-            {/* <p>{room.public}</p> */}
-            <p>{room.roomName}</p>
-            {/* <p>{room.members}</p> */}
-            {/* <p>{room.leader}</p> */}
-          </div>
-        ))}
-      </main>
-      <footer className="p-4">
-        <div className="flex justify-center space-x-2">
-          {/* 페이지 번호 버튼을 출력합니다. 여기서는 1~7까지 출력합니다. */}
-          {Array.from(
-            {
-              length: Math.ceil(
-                (tabName === "아이템" ? roomsItem.length : roomsClassic.length) / roomsPerPage
-              ),
-            },
-            (_, i) => i + 1
-          ).map((page) => (
-            <button
-              key={page}
-              onClick={() => handlePageChange(page)}
-              className="bg-blue-500 text-white p-2 rounded"
-            >
-              {page}
+    <>
+      <NavBar />
+      <div className="App">
+        <header className="flex justify-between p-4 bg-blue-500 text-white">
+          <h1>방목록</h1>
+          <div className="relative">
+            <button onClick={handleButtonClick} className="bg-green-500 p-2 rounded">
+              {tabName}
             </button>
+            {showMenu && (
+              <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="options-menu"
+                >
+                  <button
+                    onClick={() => handleItemClick("아이템")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    아이템전
+                  </button>
+                  <button
+                    onClick={() => handleItemClick("클래식")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    클래식전
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </header>
+        <main className="p-4">
+          <h2 className="text-2xl mb-4">게임 방 리스트</h2>
+          <div className="flex justify-between">
+            <p className="flex-1 text-center">공개 여부</p>
+            <p className="flex-1 text-center">방제목</p>
+            <p className="flex-1 text-center">인원</p>
+            <p className="flex-1 text-center">방장</p>
+          </div>
+
+          {currentRooms.map((room) => (
+            <div key={room.roomName} className="border p-2 mb-2 flex gap-40">
+              {/* <p>{room.public}</p> */}
+              <p>{room.roomName}</p>
+              {/* <p>{room.members}</p> */}
+              {/* <p>{room.leader}</p> */}
+            </div>
           ))}
-        </div>
-      </footer>
-    </div>
+        </main>
+        <footer className="p-4">
+          <div className="flex justify-center space-x-2">
+            {/* 페이지 번호 버튼을 출력합니다. 여기서는 1~7까지 출력합니다. */}
+            {Array.from(
+              {
+                length: Math.ceil(
+                  (tabName === "아이템" ? roomsItem.length : roomsClassic.length) / roomsPerPage
+                ),
+              },
+              (_, i) => i + 1
+            ).map((page) => (
+              <button
+                key={page}
+                onClick={() => handlePageChange(page)}
+                className="bg-blue-500 text-white p-2 rounded"
+              >
+                {page}
+              </button>
+            ))}
+          </div>
+        </footer>
+      </div>
+    </>
   );
 };
 
