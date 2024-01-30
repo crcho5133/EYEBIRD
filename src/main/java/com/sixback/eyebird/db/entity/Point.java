9d735eb449321cecf7f05cfa774c037bac6408ad
조창래 <crcho5133@naver.com>
2024-01-30 14:30:13 +0900

    Refactor: 기본 생성자 추가

--- src/main/java/com/sixback/eyebird/db/entity/Point.java
+++ src/main/java/com/sixback/eyebird/db/entity/Point.java
@@ -2,11 +2,12 @@ package com.sixback.eyebird.db.entity;
 
 import com.sixback.eyebird.api.dto.PointReqDto;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor // entity에서 기본 생성자가 필요
 @Getter
 public class Point extends BaseTime{
     @Id
@@ -22,6 +23,13 @@ public class Point extends BaseTime{
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    public Point(int classicPt, int itemPt, User user) {
+        this.classicPt = classicPt;
+        this.itemPt = itemPt;
+        this.user = user;
+    }
 
     public void update(PointReqDto pointReqDto) {
         this.classicPt += pointReqDto.getClassicPt();
