ca435dc536e5a2d5f7ce14114827321b69d663a6
조창래 <crcho5133@naver.com>
2024-01-30 14:32:29 +0900

    Refactor: 닉네임으로 유저 검색 추가

--- src/main/java/com/sixback/eyebird/db/repository/UserRepository.java
+++ src/main/java/com/sixback/eyebird/db/repository/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserByNickname(String nickname);
     Boolean existsByEmail(String email);
     Boolean existsByNickname(String nickname);
 
