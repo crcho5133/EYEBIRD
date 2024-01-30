5c5981b984048b3ce61de75bd7c89ec4625a9d00
조창래 <crcho5133@naver.com>
2024-01-30 14:28:06 +0900

    Feat: 랭크 게임 이후 점수 변동

--- src/main/java/com/sixback/eyebird/api/service/PointService.java
+++ src/main/java/com/sixback/eyebird/api/service/PointService.java
@@ -6,11 +6,13 @@ import com.sixback.eyebird.db.entity.User;
 import com.sixback.eyebird.db.repository.PointRepository;
 import com.sixback.eyebird.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PointService {
     private final UserRepository userRepository;
     private final PointRepository pointRepository;
@@ -22,6 +24,21 @@ public class PointService {
 
         // 점수 업데이트
         point.update(pointReqDto);
-
+        log.info(curUserEmail + "의 점수가 업데이트 되었습니다");
     }
+
+    // 회원 가입 시 가입된 유저의 디폴트 점수를 저장: 아이템전, 클래식전 점수 모두 디폴트 값이 1000이다
+    public void add(User signupUser) {
+        Point point = Point.builder()
+                .user(signupUser)
+                .itemPt(1000)
+                .classicPt(1000)
+                .build();
+
+
+        pointRepository.save(point);
+        log.info(signupUser.getEmail() + "의 기본 점수가 DB에 저장되었습니다");
+    }
+
+
 }
