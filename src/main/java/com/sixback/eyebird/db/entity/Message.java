eb3ed283437b2235f7af54f4643769dd387abdda
조창래 <crcho5133@naver.com>
2024-02-03 22:12:45 +0900

    Refactor: 메세지 삭제 기능

--- src/main/java/com/sixback/eyebird/db/entity/Message.java
+++ src/main/java/com/sixback/eyebird/db/entity/Message.java
@@ -1,5 +1,6 @@
 package com.sixback.eyebird.db.entity;
 
+import com.sixback.eyebird.api.dto.MessageResDto;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class Message extends BaseTime {
     @Id
     @GeneratedValue
+    private Long msgId;
     private int msgType; // 메세지의 타입: 0은 친구 추가, 1은 쪽지
     private String msgText; // 메세지의 내용
     private boolean ifRead; // 메세지가 읽혔는 지
@@ -24,7 +26,7 @@ public class Message extends BaseTime {
     private User userTo;
 
     @Builder
-    public Message(int msgType, String msgText, boolean ifRead, User userFrom, User userTo) {
+    public Message(Long msgId, int msgType, String msgText, boolean ifRead, User userFrom, User userTo) {
         this.msgType = msgType;
         this.msgText = msgText;
         this.ifRead = ifRead;
@@ -34,5 +36,14 @@ public class Message extends BaseTime {
 
     public void readMessage() {
         this.ifRead = true;
+    }
+
+    public MessageResDto toMessageResDto() {
+        return MessageResDto.builder()
+                .msgType(this.msgType)
+                .msgText(this.msgText)
+                .ifRead(this.ifRead)
+                .userFromNickname(userFrom.getNickname())
+                .build();
     }
 }
