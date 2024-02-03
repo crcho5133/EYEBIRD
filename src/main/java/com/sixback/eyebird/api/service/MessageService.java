1739c50a761ae9887db3e2badf575108e1d2c60a
조창래 <crcho5133@naver.com>
2024-02-03 22:11:37 +0900

    Refactor: 메세지 가져오기, 삭제하기

--- src/main/java/com/sixback/eyebird/api/service/MessageService.java
+++ src/main/java/com/sixback/eyebird/api/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.sixback.eyebird.api.service;
 
 import com.sixback.eyebird.api.dto.MessageReqDto;
+import com.sixback.eyebird.api.dto.MessageResDto;
 import com.sixback.eyebird.db.entity.Message;
 import com.sixback.eyebird.db.entity.User;
 import com.sixback.eyebird.db.repository.MessageRepository;
@@ -8,6 +9,9 @@ import com.sixback.eyebird.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +28,7 @@ public class MessageService {
         User userTo = userRepository.findUserByNickname(messageReqDto.getUserToNickname()).orElseThrow(() -> new IllegalArgumentException("해당 닉네임을 지닌 유저가 존재하지 않습니다"));
 
         Message message = Message.builder()
+                .msgId(messageReqDto.getMsgId())
                 .msgType(messageReqDto.getMsgType())
                 .msgText(messageReqDto.getMsgText())
                 .ifRead(false)
@@ -43,5 +48,30 @@ public class MessageService {
         message.readMessage();
         log.info("메세지 확인");
 
+    }
+
+    // 나의 메세지 모두 가져오기
+    public List<MessageResDto> getMessages(String email) {
+        User user = userRepository.findUserByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 이메일을 지닌 유저가 존재하지 않습니다"));
+
+        // 나에게 보내진 모든 메세지 가져오기
+        Long userId = user.getId();
+        List<Message> messages = messageRepository.findByUserTo_IdOrderByCreatedDate(userId);
+
+        List<MessageResDto> messageResDtoList = new ArrayList<>();
+
+        for (Message m: messages) {
+            messageResDtoList.add(m.toMessageResDto());
+        }
+
+        return messageResDtoList;
+
+    }
+
+    // 특정 메세지 삭제
+    public void deleteMessage(Long id) {
+        Message message = messageRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 메세지를 찾을 수 없습니다"));
+
+        messageRepository.deleteById(id);
     }
 }
