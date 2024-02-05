daf6dd5e8519aacf5c971113b0cf4a5fc6c5641f
jjin-21 <ljj1900@naver.com>
2024-02-05 11:06:47 +0900

    Feat : Error Test

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
       <LobbyBtn text="회원가입" onClick={isSignupModalVisible.showRodal} className="mr-4" />
       <LobbyBtn text="로그인" onClick={isLoginModalVisible.showRodal} className="mr-4" />
       <LobbyBtn text="게임설명" onClick={isDescriptionModalVisible.showRodal} />
+      <p> 테스트 </p>
       <SignupFormModal
         visible={isSignupModalVisible.value}
         onClose={isSignupModalVisible.hideRodal}
