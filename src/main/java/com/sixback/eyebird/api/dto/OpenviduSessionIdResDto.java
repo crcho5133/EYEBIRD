284d33d480f9db966ebbb3968fcf1144b76c4d55
조창래 <crcho5133@naver.com>
2024-02-05 14:44:07 +0900

    Feat: openvidu session 발급 시 상대방의 정보도 전달

--- src/main/java/com/sixback/eyebird/api/dto/OpenviduSessionIdResDto.java
+++ src/main/java/com/sixback/eyebird/api/dto/OpenviduSessionIdResDto.java
@@ -1,2 +1,55 @@
-package com.sixback.eyebird.api.dto;public class OpenviduSessionIdResDto {
+package com.sixback.eyebird.api.dto;
+
+import com.sixback.eyebird.db.entity.GameResult;
+import com.sixback.eyebird.db.entity.Point;
+import com.sixback.eyebird.db.entity.User;
+import lombok.Builder;
+
+import java.util.List;
+
+public class OpenviduSessionIdResDto {
+    private String openviduSessionId;
+    private String nickname;
+    private int profileImage;
+    private int classicPt;
+    private int itemPt;
+    private int winNumItem;
+    private int loseNumItem;
+    private int winNumClassic;
+    private int loseNumClassic;
+
+    @Builder
+    public OpenviduSessionIdResDto(User user, String openviduSessionId) {
+        this.openviduSessionId = openviduSessionId;
+
+        this.nickname = user.getNickname();
+        this.profileImage = user.getProfileImage();
+
+        Point point = user.getPoint();
+        this.classicPt = point.getClassicPt();
+        this.itemPt = point.getItemPt();
+
+        List<GameResult> winGameResults = user.getWinGameResults();
+        List<GameResult> loseGameResults = user.getLoseGameResults();
+
+        int winNum = winGameResults.size();
+        int loseNum = loseGameResults.size();
+
+        int winNumItem = 0;
+        int loseNumItem = 0;
+
+        for (GameResult g: winGameResults) {
+            if (g.isIfItem()) winNumItem++;
+        }
+
+        for (GameResult g: loseGameResults) {
+            if (g.isIfItem()) loseNumItem++;
+        }
+
+        this.winNumItem = winNumItem;
+        this.loseNumItem = loseNumItem;
+        this.winNumClassic = winNum - winNumItem;
+        this.loseNumClassic = loseNum - loseNumItem;
+
+    }
 }
