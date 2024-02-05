49880e5cf0a4de2cece52dd96a051128d3069f44
장수영 <suyoung998@naver.com>
2024-02-05 16:04:23 +0900

    Feat : 배포 수정

--- S10P12E206-develop-BE/src/main/java/com/sixback/eyebird/api/dto/MatchingGameReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.sixback.eyebird.api.dto;
-
-import lombok.Getter;
-
-@Getter
-public class MatchingGameReqDto {
-    // 매칭이 성공된 후의 openvidu
-    private String gameId;
-}
--- S10P12E206-develop-BE/src/main/java/com/sixback/eyebird/db/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sixback.eyebird.db.repository;
-
-import com.sixback.eyebird.db.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
