954f1914258c18c064f3505a3cea439d0fe095f7
조창래 <crcho5133@naver.com>
2024-02-06 14:48:06 +0900

    Refactor: 친구 요청 메세지 찾기

--- src/main/java/com/sixback/eyebird/db/repository/MessageRepository.java
+++ src/main/java/com/sixback/eyebird/db/repository/MessageRepository.java
@@ -5,7 +5,9 @@ import com.sixback.eyebird.db.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByUserTo_IdOrderByCreatedDate(Long id);
+   Optional<Message> findByUserFrom_IdAndUserTo_IdAndMsgType(Long fromId, Long toId, int msgType);
 }
