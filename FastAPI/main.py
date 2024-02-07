196801cc63c9efd9101478deb7b5feb8723f470e
jjin-21 <ljj1900@naver.com>
2024-02-07 23:49:40 +0900

    Feat : FastAPI

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, WebSocket
 import cv2
 import mediapipe as mp
 import numpy as np
-from concurrent.futures import ThreadPoolExecutor
-import asyncio
 
 app = FastAPI()
 
@@ -31,60 +29,48 @@ def eye_aspect_ratio(eye):
     ear = (vertical_1 + vertical_2 + vertical_3 + vertical_4) / (3.0 * horizontal)
     return ear
 
-# 이미지 처리를 별도의 스레드에서 실행하는 함수
-def process_image(data):
-    image = np.asarray(bytearray(data), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    
-    # MediaPipe로 얼굴 랜드마크 처리
-    results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    
-    if results.multi_face_landmarks:
-        for face_landmarks in results.multi_face_landmarks:
-            # 왼쪽 눈 랜드마크 추출
-            left_eye_upper_landmarks = [face_landmarks.landmark[i] for i in [173, 157, 158, 159, 160, 161, 246]] # 왼쪽 눈 위
-            left_eye_lower_landmarks = [face_landmarks.landmark[i] for i in [155, 154, 153, 145, 144, 163, 7]] # 왼쪽 눈 아래
-            left_eye_inner_landmarks = [face_landmarks.landmark[133]] # 왼쪽 눈 안쪽
-            left_eye_outer_landmarks = [face_landmarks.landmark[33]] # 왼쪽 눈 바깥쪽
-            
-            # 오른쪽 눈 랜드마크 추출
-            right_eye_upper_landmarks = [face_landmarks.landmark[i] for i in [398, 384, 385, 386, 387, 388, 466]] # 오른쪽 눈 위
-            right_eye_lower_landmarks = [face_landmarks.landmark[i] for i in [382, 381, 380, 374, 373, 390, 249]] # 오른쪽 눈 아래
-            right_eye_inner_landmarks = [face_landmarks.landmark[362]] # 오른쪽 눈 안쪽
-            right_eye_outer_landmarks = [face_landmarks.landmark[263]] # 오른쪽 눈 바깥쪽
-            
-            left_eye = np.array([(landmark.x, landmark.y) for landmark in left_eye_upper_landmarks + left_eye_lower_landmarks + left_eye_inner_landmarks + left_eye_outer_landmarks])
-            right_eye = np.array([(landmark.x, landmark.y) for landmark in right_eye_upper_landmarks + right_eye_lower_landmarks + right_eye_inner_landmarks + right_eye_outer_landmarks])
-            
-            left_EAR = eye_aspect_ratio(left_eye)
-            right_EAR = eye_aspect_ratio(right_eye)
-            
-            # 눈 감김 판단
-            if left_EAR < 0.15 or right_EAR < 0.15:
-                return (str(left_EAR) + "\n" + str(right_EAR) + "\n" + "눈 감김 감지됨")
-            else:
-                return (str(left_EAR) + "\n" + str(right_EAR) + "\n" + "눈 감김 감지 안 됨")
-
-
-@app.get("/fastpi")
+@app.get("/fastapi")
 async def get():
-    return {"message": "Hello World"}
+    return {"message": "test"}
 
 @app.websocket("/fastapi/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    await websocket.send_text("연결 성공")
-    
-    # 스레드 풀 실행자 생성
-    executor = ThreadPoolExecutor(max_workers=4)
-    
+    await websocket.send_text("연결성공")
+
     while True:
         data = await websocket.receive_bytes()
+        image = np.asarray(bytearray(data), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         
-        loop = asyncio.get_running_loop()
-        # 비동기적으로 이미지 처리 함수를 실행
-        result = await loop.run_in_executor(executor, process_image, data)
+        # MediaPipe로 얼굴 랜드마크 처리
+        results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         
-        # 처리 결과를 웹소켓을 통해 클라이언트에 전송
-        result_str = str(result)  # 결과를 문자열로 변환
-        await websocket.send_text(result_str)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                # 왼쪽 눈 랜드마크 추출
+                left_eye_upper_landmarks = [face_landmarks.landmark[i] for i in [173, 157, 158, 159, 160, 161, 246]] # 왼쪽 눈 위
+                left_eye_lower_landmarks = [face_landmarks.landmark[i] for i in [155, 154, 153, 145, 144, 163, 7]] # 왼쪽 눈 아래
+                left_eye_inner_landmarks = [face_landmarks.landmark[133]] # 왼쪽 눈 안쪽
+                left_eye_outer_landmarks = [face_landmarks.landmark[33]] # 왼쪽 눈 바깥쪽
+                
+                # 오른쪽 눈 랜드마크 추출
+                right_eye_upper_landmarks = [face_landmarks.landmark[i] for i in [398, 384, 385, 386, 387, 388, 466]] # 오른쪽 눈 위
+                right_eye_lower_landmarks = [face_landmarks.landmark[i] for i in [382, 381, 380, 374, 373, 390, 249]] # 오른쪽 눈 아래
+                right_eye_inner_landmarks = [face_landmarks.landmark[362]] # 오른쪽 눈 안쪽
+                right_eye_outer_landmarks = [face_landmarks.landmark[263]] # 오른쪽 눈 바깥쪽
+                
+                left_eye = np.array([(landmark.x, landmark.y) for landmark in left_eye_upper_landmarks + left_eye_lower_landmarks + left_eye_inner_landmarks + left_eye_outer_landmarks])
+                right_eye = np.array([(landmark.x, landmark.y) for landmark in right_eye_upper_landmarks + right_eye_lower_landmarks + right_eye_inner_landmarks + right_eye_outer_landmarks])
+                
+                left_EAR = eye_aspect_ratio(left_eye)
+                right_EAR = eye_aspect_ratio(right_eye)
+                
+                # 눈 감김 판단
+                if left_EAR < 0.15 or right_EAR < 0.15:
+                    await websocket.send_text(str(left_EAR) + "\n" + str(right_EAR) + "\n" + "눈 감김 감지됨")
+                    # await websocket.send_text("check")
+                else:
+                    await websocket.send_text(str(left_EAR) + "\n" + str(right_EAR) + "\n" + "눈 감김 감지 안 됨")
+                # else:
+                #     await websocket.send_text("xxx")
