14f55cf6b8cf763062efe74caa29529134ec124e
조창래 <crcho5133@naver.com>
2024-02-08 14:09:59 +0900

    Update BE/src/main/java/com/sixback/eyebird/config/RedisConfig.java

--- BE/src/main/java/com/sixback/eyebird/config/RedisConfig.java
+++ BE/src/main/java/com/sixback/eyebird/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -21,7 +22,7 @@ public class RedisConfig {
     private String password;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
+    public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
         redisConfig.setPassword(password);
 
@@ -41,4 +42,5 @@ public class RedisConfig {
 
         return redisTemplate;
     }
+}
 
