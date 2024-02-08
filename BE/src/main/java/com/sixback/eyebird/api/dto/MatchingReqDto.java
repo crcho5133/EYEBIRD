e8119616941abdebf48a3ab14c85c451fc094592
장수영 <suyoung998@naver.com>
2024-02-08 11:16:34 +0900

    Merge : BE

--- BE/src/main/java/com/sixback/eyebird/api/dto/MatchingReqDto.java
+++ BE/src/main/java/com/sixback/eyebird/api/dto/MatchingReqDto.java
@@ -14,4 +14,15 @@ public class MatchingReqDto {
     @NotBlank(message="매칭 요청: 매칭 요청한 유저의 이메일은 빈 문자열이 되어선 안 됩니다")
     String email;
 
+    // 유저의 현재 점수
+    int point;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MatchingReqDto matchingReqDto = (MatchingReqDto) obj;
+        return ifItem == matchingReqDto.isIfItem() && this.email.equals(matchingReqDto.email) && this.point == matchingReqDto.point;
+    }
+
 }
--- BE/src/main/java/com/sixback/eyebird/api/dto/RoomDto.java
+++ BE/src/main/java/com/sixback/eyebird/api/dto/RoomDto.java
@@ -1,36 +1,24 @@
 package com.sixback.eyebird.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 public class RoomDto {
     private String roomId;          // 방의 고유 식별자 room_ + roomId 형식으로 key 제작
     private String roomName;        // 방의 제목
     private boolean item;           // 아이템전 여부
-    private boolean hasPassword;
-                                    // Rombok에서 isItem Getter를 만들기 때문에 Redis에 item으로 저장되는 버그가 있어서 Dto를 item으로 변경함.
+    // Rombok에서 isItem Getter를 만들기 때문에 Redis에 item으로 저장되는 버그가 있어서 Dto를 item으로 변경함.
+    private boolean hasPassword;    // 비밀번호방 여부
+    private boolean status;         // 대기중, 게임중의 두 가지 상태
+
     private int maxCapacity;        // 최대 인원수
     private int currentCapacity;    // 현재 인원수
+
     private int password;           // 방 접근을 위한 비밀번호 (선택적)
     private long ownerId;           // 방장 ID
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "roomId='" + roomId + '\'' +
-                ", roomName='" + roomName + '\'' +
-                ", maxCapacity=" + maxCapacity +
-                ", currentCapacity=" + currentCapacity +
-                ", password=" + password +
-                ", ownerId='" + ownerId + '\'' +
-                ", item=" + item +
-                '}';
-    }
 
     public void addCapacity(){
         currentCapacity += 1;
@@ -46,6 +34,9 @@ public class RoomDto {
     }
     public void setHasPassword(boolean has){
         this.hasPassword = has;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
 
