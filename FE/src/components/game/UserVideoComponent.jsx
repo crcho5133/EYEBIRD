e7ab99f4bbbeda5f0859da125366889618f0dd8c
심상익 <tkddlrtla@naver.com>
2024-02-14 22:53:50 +0900

    Feat: 얼굴 랜드마크 모델 변경(tasks-vision)

--- FE/src/components/game/UserVideoComponent.jsx
+++ FE/src/components/game/UserVideoComponent.jsx
@@ -4,11 +4,12 @@ import OpenViduVideoComponent from "./OvVideo";
 import { useWebSocket } from "../../context/VideoWebSocketContext";
 import nickname_plate from "../../assets/img/nickname_plate.png";
 import frame from "../../assets/img/frame.png";
-import "@mediapipe/face_mesh";
-import * as tf from "@tensorflow/tfjs-core";
-// Register WebGL backend.
-import "@tensorflow/tfjs-backend-webgl";
-import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+// // import "@mediapipe/face_mesh";
+// import * as tf from "@tensorflow/tfjs-core";
+// // Register WebGL backend.
+// import "@tensorflow/tfjs-backend-webgl";
+// import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
 
 function euclideanDistance(pointA, pointB) {
   const distance = Math.sqrt(
@@ -45,83 +46,164 @@ export default function UserVideoComponent({
   // const { socket, message } = useWebSocket();
   const myInfo = useAccessTokenState();
 
+  const faceLandmarker = useRef(undefined);
+
   useEffect(() => {
     if (streamManager && gameState === "play") {
-      tf.setBackend("webgl").then(async () => {
-        const model = await faceLandmarksDetection.createDetector(
-          faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh,
-          {
-            runtime: "mediapipe",
-            solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh",
-            maxFaces: 1,
-            refineLandmarks: true,
-            minDetectionConfidence: 0.5,
-            minTrackingConfidence: 0.5,
-          }
+      async function createFaceLandmarker() {
+        const filesetResolver = await FilesetResolver.forVisionTasks(
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
         );
+        faceLandmarker.current = await FaceLandmarker.createFromOptions(filesetResolver, {
+          baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+            delegate: "GPU",
+          },
+          // outputFaceBlendshapes: true,
+          runningMode: "VIDEO",
+          numFaces: 1,
+        });
+      }
+      createFaceLandmarker();
+      const detect = async () => {
+        if (videoRef.current && videoRef.current.readyState >= 4 && faceLandmarker.current) {
+          const faceLandmarkerResult = await faceLandmarker.current.detectForVideo(
+            videoRef.current,
+            performance.now()
+          );
+          if (faceLandmarkerResult.faceLandmarks.length > 0) {
+            // console.log(faceLandmarkerResult);
+            const faceLandmarks = faceLandmarkerResult.faceLandmarks[0];
+            // 왼쪽 눈
+            const leftEyeUpperLandmarks = [173, 157, 158, 159, 160, 161, 246].map(
+              (i) => faceLandmarks[i]
+            );
+            const leftEyeLowerLandmarks = [155, 154, 153, 145, 144, 163, 7].map(
+              (i) => faceLandmarks[i]
+            );
+            const leftEyeInnerLandmarks = [faceLandmarks[133]];
+            const leftEyeOuterLandmarks = [faceLandmarks[33]];
 
-        const detect = async () => {
-          if (videoRef.current && videoRef.current.readyState === 4) {
-            // 비디오의 현재 프레임에서 얼굴 랜드마크 감지
-            const faces = await model.estimateFaces(videoRef.current, { flipHorizontal: false });
-            if (faces.length > 0) {
-              // 감지된 얼굴 랜드마크 정보 출력
-              // console.log(faces[0]);
-              const faceLandmarks = faces[0];
-              // 왼쪽 눈
-              const leftEyeUpperLandmarks = [173, 157, 158, 159, 160, 161, 246].map(
-                (i) => faceLandmarks.keypoints[i]
-              );
-              const leftEyeLowerLandmarks = [155, 154, 153, 145, 144, 163, 7].map(
-                (i) => faceLandmarks.keypoints[i]
-              );
-              const leftEyeInnerLandmarks = [faceLandmarks.keypoints[133]];
-              const leftEyeOuterLandmarks = [faceLandmarks.keypoints[33]];
+            // 오른쪽 눈
+            const rightEyeUpperLandmarks = [398, 384, 385, 386, 387, 388, 466].map(
+              (i) => faceLandmarks[i]
+            );
+            const rightEyeLowerLandmarks = [382, 381, 380, 374, 373, 390, 249].map(
+              (i) => faceLandmarks[i]
+            );
+            const rightEyeInnerLandmarks = [faceLandmarks[362]];
+            const rightEyeOuterLandmarks = [faceLandmarks[263]];
 
-              // 오른쪽 눈
-              const rightEyeUpperLandmarks = [398, 384, 385, 386, 387, 388, 466].map(
-                (i) => faceLandmarks.keypoints[i]
-              );
-              const rightEyeLowerLandmarks = [382, 381, 380, 374, 373, 390, 249].map(
-                (i) => faceLandmarks.keypoints[i]
-              );
-              const rightEyeInnerLandmarks = [faceLandmarks.keypoints[362]];
-              const rightEyeOuterLandmarks = [faceLandmarks.keypoints[263]];
+            const leftEyeLandmarks = [
+              ...leftEyeUpperLandmarks,
+              ...leftEyeLowerLandmarks,
+              ...leftEyeInnerLandmarks,
+              ...leftEyeOuterLandmarks,
+            ];
+            const leftEye = leftEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
+            const rightEyeLandmarks = [
+              ...rightEyeUpperLandmarks,
+              ...rightEyeLowerLandmarks,
+              ...rightEyeInnerLandmarks,
+              ...rightEyeOuterLandmarks,
+            ];
+            const rightEye = rightEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
 
-              const leftEyeLandmarks = [
-                ...leftEyeUpperLandmarks,
-                ...leftEyeLowerLandmarks,
-                ...leftEyeInnerLandmarks,
-                ...leftEyeOuterLandmarks,
-              ];
-              const leftEye = leftEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
-              const rightEyeLandmarks = [
-                ...rightEyeUpperLandmarks,
-                ...rightEyeLowerLandmarks,
-                ...rightEyeInnerLandmarks,
-                ...rightEyeOuterLandmarks,
-              ];
-              const rightEye = rightEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
+            const leftEAR = calculateEAR(leftEye);
+            const rightEAR = calculateEAR(rightEye);
 
-              const leftEAR = calculateEAR(leftEye);
-              const rightEAR = calculateEAR(rightEye);
-
-              if (leftEAR < 0.15 || rightEAR < 0.15) {
-                console.log("눈 감음");
-                console.log(`Left Eye EAR: ${leftEAR}, Right Eye EAR: ${rightEAR}`);
-                sendLose();
-              }
-              // console.log(`Left Eye EAR: ${leftEAR}, Right Eye EAR: ${rightEAR}`);
+            if (leftEAR < 0.12 || rightEAR < 0.12) {
+              console.log("눈 감음");
+              console.log(`Left Eye EAR: ${leftEAR}, Right Eye EAR: ${rightEAR}`);
+              sendLose();
             }
-            // requestAnimationFrame(detect); // 다음 프레임을 위해 detect 함수를 계속 호출
           }
-        };
-        // detect();
-        const intervalId = setInterval(detect, 1000 / 30);
-        return () => clearInterval(intervalId);
-      });
+        }
+        // requestAnimationFrame(detect);
+      };
+      // detect();
+      const intervalId = setInterval(detect, 1000 / 30);
+      return () => clearInterval(intervalId);
     }
   }, [streamManager]);
+
+  // useEffect(() => {
+  //   if (streamManager && gameState === "play") {
+  //     tf.setBackend("webgl").then(async () => {
+  //       const model = await faceLandmarksDetection.createDetector(
+  //         faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh,
+  //         {
+  //           runtime: "mediapipe",
+  //           solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh",
+  //           maxFaces: 1,
+  //           refineLandmarks: true,
+  //           minDetectionConfidence: 0.5,
+  //           minTrackingConfidence: 0.5,
+  //         }
+  //       );
+
+  //       const detect = async () => {
+  //         if (videoRef.current && videoRef.current.readyState === 4) {
+  //           // 비디오의 현재 프레임에서 얼굴 랜드마크 감지
+  //           const faces = await model.estimateFaces(videoRef.current, { flipHorizontal: false });
+  //           if (faces.length > 0) {
+  //             // 감지된 얼굴 랜드마크 정보 출력
+  //             // console.log(faces[0]);
+  //             const faceLandmarks = faces[0];
+  //             // 왼쪽 눈
+  //             const leftEyeUpperLandmarks = [173, 157, 158, 159, 160, 161, 246].map(
+  //               (i) => faceLandmarks.keypoints[i]
+  //             );
+  //             const leftEyeLowerLandmarks = [155, 154, 153, 145, 144, 163, 7].map(
+  //               (i) => faceLandmarks.keypoints[i]
+  //             );
+  //             const leftEyeInnerLandmarks = [faceLandmarks.keypoints[133]];
+  //             const leftEyeOuterLandmarks = [faceLandmarks.keypoints[33]];
+
+  //             // 오른쪽 눈
+  //             const rightEyeUpperLandmarks = [398, 384, 385, 386, 387, 388, 466].map(
+  //               (i) => faceLandmarks.keypoints[i]
+  //             );
+  //             const rightEyeLowerLandmarks = [382, 381, 380, 374, 373, 390, 249].map(
+  //               (i) => faceLandmarks.keypoints[i]
+  //             );
+  //             const rightEyeInnerLandmarks = [faceLandmarks.keypoints[362]];
+  //             const rightEyeOuterLandmarks = [faceLandmarks.keypoints[263]];
+
+  //             const leftEyeLandmarks = [
+  //               ...leftEyeUpperLandmarks,
+  //               ...leftEyeLowerLandmarks,
+  //               ...leftEyeInnerLandmarks,
+  //               ...leftEyeOuterLandmarks,
+  //             ];
+  //             const leftEye = leftEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
+  //             const rightEyeLandmarks = [
+  //               ...rightEyeUpperLandmarks,
+  //               ...rightEyeLowerLandmarks,
+  //               ...rightEyeInnerLandmarks,
+  //               ...rightEyeOuterLandmarks,
+  //             ];
+  //             const rightEye = rightEyeLandmarks.map((landmark) => [landmark.x, landmark.y]);
+
+  //             const leftEAR = calculateEAR(leftEye);
+  //             const rightEAR = calculateEAR(rightEye);
+
+  //             if (leftEAR < 0.15 || rightEAR < 0.15) {
+  //               console.log("눈 감음");
+  //               console.log(`Left Eye EAR: ${leftEAR}, Right Eye EAR: ${rightEAR}`);
+  //               sendLose();
+  //             }
+  //             // console.log(`Left Eye EAR: ${leftEAR}, Right Eye EAR: ${rightEAR}`);
+  //           }
+  //           // requestAnimationFrame(detect); // 다음 프레임을 위해 detect 함수를 계속 호출
+  //         }
+  //       };
+  //       // detect();
+  //       const intervalId = setInterval(detect, 1000 / 30);
+  //       return () => clearInterval(intervalId);
+  //     });
+  //   }
+  // }, [streamManager]);
 
   // useEffect(() => {
   //   if (streamManager && socket && gameState === "play") {
